617c341d6a37ab1d177dabbc04b842c6a7ce4909
Oliver Schupke <solanx@web.de>
2024-05-15 13:31:11 +0200

    Refactor: Implement useEffect() for fetching weather data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ActivityForm from "./components/ActivityForm/ActivityForm";
 import { uid } from "uid";
 import "./App.css";
@@ -22,6 +22,32 @@ export default function App() {
 	function handleAddActivity(newActivity) {
 		setActivities([{ id: uid(), ...newActivity }, ...activities]);
 	}
+
+	const [isGoodWeather, setIsGoodWeather] = useState(true);
+	const [condition, setCondition] = useState("");
+	const [temperature, setTemperature] = useState("");
+
+	async function fetchWeather() {
+		try {
+			const response = await fetch("https://example-apis.vercel.app/api/weather");
+			if (!response.ok)
+				throw new Error(`Failed to fetch weather data, status code: ${response.status}`);
+			const weather = await response.json();
+
+			setIsGoodWeather(weather?.isGoodWeather);
+			setCondition(weather?.condition);
+			setTemperature(weather?.temperature);
+		} catch (error) {
+			console.error("Fetch error:", error);
+		}
+	}
+
+	useEffect(() => {
+		const intervalId = setInterval(fetchWeather, 5000);
+
+		console.log("hello");
+		return () => clearInterval(intervalId);
+	}, []);
 
 	return (
 		<>
