fe79a5c5b86858ff3bb6b77df2784ffc4363705b
M6K4N9R <zubchevskiy@gmail.com>
2024-05-15 09:49:57 +0200

    Install uid

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,29 @@ import { uid } from "uid";
 import "./App.css";
 
 const initialActivities = [
-	{
-		id: 1,
-		name: "badminton",
-		isForGoodWeather: true,
-	},
-	{
-		id: 2,
-		name: "chess",
-		isForGoodWeather: false,
-	},
+  {
+    id: 1,
+    name: "badminton",
+    isForGoodWeather: true,
+  },
+  {
+    id: 2,
+    name: "chess",
+    isForGoodWeather: false,
+  },
 ];
 
 export default function App() {
-	const [activities, setActivities] = useState(initialActivities);
+  const [activities, setActivities] = useState(initialActivities);
 
-	function handleAddActivity(newActivity) {
-		setActivities([{ id: uid(), ...newActivity }, ...activities]);
-	}
+  function handleAddActivity(newActivity) {
+    setActivities([{ id: uid(), ...newActivity }, ...activities]);
+  }
 
-	return (
-		<>
-			<h1>Weather App</h1>
-			<ActivityForm onAddActivity={handleAddActivity} />
-		</>
-	);
+  return (
+    <>
+      <h1>Weather App</h1>
+      <ActivityForm onAddActivity={handleAddActivity} />
+    </>
+  );
 }
